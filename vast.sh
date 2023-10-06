330d0add86f037a937aba024271719b0ecaf8064
July-Jio <80434227+July-Jio@users.noreply.github.com>
2023-10-06 17:56:40 +0800

    Update vast.sh
    update window spslit

--- vast.sh
+++ vast.sh
@@ -27,6 +27,10 @@ sudo apt -y install python3-pip
 sleep 5
 echo "---------------------------"
 
+sudo apt install tmux
+sleep 5
+echo "--------------------------"
+
 # Clone the repository and build the project
 git clone https://github.com/shanhaicoder/XENGPUMiner.git
 sleep 5
@@ -40,7 +44,7 @@ chmod +x build.sh
 sleep 5
 echo "---------------------------"
 
-./build.sh
+./build.sh -cuda_arch sm_89
 sleep 5
 echo "---------------------------"
 
@@ -54,10 +58,30 @@ sudo pip install -U -r requirements.txt
 sleep 5
 echo "---------------------------"
 
-sudo nohup python3 miner.py --gpu=true > miner.log 2>&1 &
-sleep 5
-echo "---------------------------"
+#  original cmd
+# sudo nohup python3 miner.py --gpu=true > miner.log 2>&1 &
+# sleep 5
+# echo "---------------------------"
 
-sudo nohup ./xengpuminer > xengpuminer.log 2>&1 &
-sleep 5
-echo "---------------------------"
+# sudo nohup ./xengpuminer > xengpuminer.log 2>&1 &
+# sleep 5
+# echo "---------------------------"
+
+# Create split window
+# Create a new tmux session, but don't attach to it yet
+sudo tmux new-session -d -s gpuminer
+
+# Split the window horizontally
+sudo tmux split-window -v
+
+# Run the Python miner command in the top pane (pane 0)
+sudo tmux send-keys -t gpuminer:0.0 'python3 miner.py --gpu=true' C-m
+
+# Wait for 3 seconds
+sleep 3
+
+# Run the GPU miner command in the bottom pane (pane 1)
+sudo tmux send-keys -t gpuminer:0.1 './xengpuminer' C-m
+
+# Finally, attach to the tmux session
+sudo tmux attach -t gpuminer
